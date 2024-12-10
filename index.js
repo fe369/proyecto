97497cf4973a9fca368354d496d2515fee293541
fe369 <jer06doc@gmail.com>
2024-12-09 23:05:31 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
+import User from "./user.js";
+import Task from "./tasks.js";
+import db from "../config.js";
 
-import express from 'express';
-import db from './models/index.js';
-import routes from './routes/index.js';
-
-const app = express();
-const PORT = 3000;
-
-// Middleware para manejar JSON
-app.use(express.json());
-
-// Rutas
-app.use('/api', routes);
-
-// Conectar a la base de datos
-db.sync({ force: false });
-
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const user = db.define('User', User, {timestamps: false, createdAt: false});
+const task = db.define('Task', Task, {timestamps: false, createdAt: false});
+user.hasMany(task, {
+    foreignKey: 'user_id'
 });
+
+export default db
